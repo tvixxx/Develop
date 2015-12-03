729670e4c92fc338fb2519203cdf2d8d16d1bd5f
tvixxx <tvix2000@yandex.ru>
2015-12-03 18:55:09 +0400

    finished

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     <script src="js/js.js"></script>
     <link rel="stylesheet" href="fonts/icomoon/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/media.css">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
